0dc03f2ce42cedf1283a82e5c38d1b90f4a5edda
BitFloyd <sby.jcub@gmail.com>
2020-05-26 10:01:36 -0400

    save models.

--- train_traffic_sign_detector/export_model.sh
+++ train_traffic_sign_detector/export_model.sh
@@ -1,6 +1,6 @@
-PIPELINE_CONFIG_PATH=/Users/sebyjacob/My_Projects/CanIParkHere/train_traffic_sign_detector/tensorflow_object_detection/models/ssd_mobilenet_v2_coco.config
-MODEL_CHECKPOINT_PREFIX=/Users/sebyjacob/My_Projects/CanIParkHere/train_traffic_sign_detector/tensorflow_object_detection/models/ssd_mobilenet_v2_coco_tr/trained_model/model.ckpt-50000
-SAVE_EXPORTED_PATH=/Users/sebyjacob/My_Projects/CanIParkHere/train_traffic_sign_detector/tensorflow_object_detection/models/ssd_mobilenet_v2_coco_tr/trained_model/exported_graph
+PIPELINE_CONFIG_PATH=${HOME}/CanIParkHere/train_traffic_sign_detector/tensorflow_object_detection/models/ssd_mobilenet_v2_coco.config
+MODEL_CHECKPOINT_PREFIX=${HOME}/CanIParkHere/train_traffic_sign_detector/tensorflow_object_detection/models/ssd_mobilenet_v2_coco_tr/trained_model/model.ckpt-50000
+SAVE_EXPORTED_PATH=${HOME}/CanIParkHere/train_traffic_sign_detector/tensorflow_object_detection/models/ssd_mobilenet_v2_coco_tr/trained_model/exported_graph
 
 python3 export_inference_graph.py \
     --input_type image_tensor \
